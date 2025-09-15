f20d7747426dd57b48b5168009b3e5ea77d9b5c9
vishnuvardhan <234g1a33j1@srit.ac.in>
2025-09-15 12:14:31 +0530

    full stack

---
+++ my-counter-app/src/temp.jsx
@@ -0,0 +1,11 @@
+function welcome(prop) {
+    return<h2>welcome{prop.anme}!</h2>
+    funtion temp(){
+        return(
+            <div>
+                <welcome
+
+            </div>
+        )
+    }
+}
